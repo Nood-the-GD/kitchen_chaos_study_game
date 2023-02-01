7bd12d9d4ab9bba9a4cd634cbcc690602411c426
Tuong <tuongnt@vitalify.asia>
2023-02-01 14:40:28 +0700

    add animation controller, cinemachine camera

--- kitchen_chaos/Assets/Scripts/Player/Player.cs
+++ kitchen_chaos/Assets/Scripts/Player/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -34,5 +36,14 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
