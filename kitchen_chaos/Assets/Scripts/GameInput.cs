2d7c09310f29f47be0b1f129dff8a7e098b26703
Tuong <tuongnt@vitalify.asia>
2023-02-01 15:08:13 +0700

    add new input system

---
+++ kitchen_chaos/Assets/Scripts/GameInput.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputAction playerInputAction;
+
+    private void Awake()
+    {
+        playerInputAction = new PlayerInputAction();
+        playerInputAction.Player.Enable();
+    }
+
+    public Vector2 MovementVectorNormalize()
+    {
+        Vector2 inputVector = playerInputAction.Player.Move.ReadValue<Vector2>();
+
+        return inputVector.normalized;
+    }
+}
--- kitchen_chaos/Assets/Scripts/Player/Player.cs
+++ kitchen_chaos/Assets/Scripts/Player/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
@@ -17,21 +18,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        Vector2 inputVector = new Vector2();
-        if(Input.GetKey(KeyCode.W)){
-            inputVector.y = +1;
-        }        
-        if(Input.GetKey(KeyCode.S)){
-            inputVector.y = -1;
-        }        
-        if(Input.GetKey(KeyCode.A)){
-            inputVector.x = -1;
-        }        
-        if(Input.GetKey(KeyCode.D)){
-            inputVector.x = +1;
-        }        
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.MovementVectorNormalize();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
