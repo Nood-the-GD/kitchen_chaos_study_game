9be5cd7ebb1ece37ac2e7ea15787e82c9bf94366
tuong-student <62384988+tuong-student@users.noreply.github.com>
2023-02-02 01:25:34 +0700

    Create ScriptableObject for KitchenObject

--- kitchen_chaos/Assets/Scripts/Couter/ClearCounter.cs
+++ kitchen_chaos/Assets/Scripts/Couter/ClearCounter.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact()
     {
-        Debug.Log("Interact");
+        Transform kitchenObjectTransform = Instantiate(this.kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        KitchenObjectSO kitchenObjectSO = kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO();
+        Debug.Log(kitchenObjectSO.name);
     }
+
+
 }
