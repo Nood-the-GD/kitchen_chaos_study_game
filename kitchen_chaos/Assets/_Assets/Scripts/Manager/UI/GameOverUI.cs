6738ca080cb19327edadb21c350d99afe2c137bf
Tuong <tuongnt@vitalify.asia>
2024-04-26 23:40:04 +0700

    delete game over ui code

--- kitchen_chaos/Assets/_Assets/Scripts/Manager/UI/GameOverUI.cs
+++ kitchen_chaos/Assets/_Assets/Scripts/Manager/UI/GameOverUI.cs
@@ -8,8 +8,6 @@ public class GameOverUI : MonoBehaviour
 {
     private void Start()
     {
-        GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
-        gameObject.SetActive(false);
     }
 
     private void Update()
@@ -18,12 +16,5 @@ public class GameOverUI : MonoBehaviour
 
     private void GameManager_OnStateChanged(object sender, System.EventArgs e)
     {
-        if(GameManager.Instance.IsGameOver() && PhotonNetwork.IsMasterClient)
-        {
-            PhotonManager.s.CmdEndGame();
-        }
-        else
-        {
-        }
     }
 }
