78b2952cf0a4a9b28e23bbb9f09a8209385cb5da
Tuong <tuongnt@vitalify.asia>
2024-05-24 14:23:01 +0700

    complete moving with AI

---
+++ kitchen_chaos/Assets/_Assets/Scripts/AI/TestAI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestAI : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private BaseCounter _baseCounter;
+
+    private void Start()
+    {
+        // _agent.SetDestination(new Vector3(0, 0, 0));
+    }
+
+    void Update()
+    {
+        // Vector3 dir = _baseCounter.transform.position - this.transform.position;
+        // dir.Normalize();
+        // Vector3 newPos = this.transform.position + dir * 2f;
+        if(NavMesh.SamplePosition(_baseCounter.transform.position, out NavMeshHit hit, 100f, NavMesh.AllAreas))
+        {
+            Debug.Log("True");
+            _agent.SetDestination(hit.position);
+            // _agent.transform.position = hit.position;
+        }
+    }
+}
