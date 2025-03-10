5b91a4daa70fac0cd2ac2cb6f37c8a0b39379282
Noody (NoodyAbility) <62384988+Nood-the-GD@users.noreply.github.com>
2025-03-10 22:41:21 +0700

    feat (customer): add customer

---
+++ kitchen_chaos/Assets/_Assets/Scripts/Customer/CustomerOld.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using Photon.Pun;
+using UnityEngine;
+
+public class CustomerOld : MonoBehaviour
+{
+    private PhotonView _photonView;
+    private CustomerAnimator _customerAnimator;
+    private float _speed = 3f;
+    private bool _isBlocked = false;
+    private bool _forceMove = false;
+
+    void Awake()
+    {
+        _photonView = GetComponent<PhotonView>();
+        _customerAnimator = GetComponentInChildren<CustomerAnimator>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        _isBlocked = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _isBlocked = false;
+    }
+
+    private async void OnRecipeSuccess(object sender, EventArgs e)
+    {
+        if (CustomerSpawner.s.IsTopList(this))
+        {
+            CustomerSpawner.s.RemoveCustomerOld(this);
+            await Eat();
+            Leave();
+        }
+    }
+
+    public async UniTask Move(Vector3 position)
+    {
+        var distance = Vector3.Distance(transform.position, position);
+        while (distance > 0.1f)
+        {
+            if (_isBlocked && _forceMove == false)
+            {
+                await UniTask.DelayFrame(10);
+                _customerAnimator.Stop();
+                continue;
+            }
+            _customerAnimator.Walk();
+            Vector3 dir = (position - transform.position).normalized;
+            transform.position += dir * Time.deltaTime * _speed;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * _speed);
+            distance = Vector3.Distance(transform.position, position);
+            await UniTask.DelayFrame(1);
+        }
+        if (PhotonNetwork.IsMasterClient)
+            DeliveryManager.Instance.OnRecipeSuccess += OnRecipeSuccess;
+        _customerAnimator.Stop();
+    }
+
+    public async UniTask Eat()
+    {
+        _customerAnimator.Roll();
+        await UniTask.WaitForSeconds(_customerAnimator.Animator.GetAnimationLength("Roll"));
+    }
+
+    private async UniTask Leave()
+    {
+        _customerAnimator.Walk();
+        DeliveryManager.Instance.OnRecipeSuccess -= OnRecipeSuccess;
+        _forceMove = true;
+        await Move(this.transform.position - new Vector3(0, 0, 20f));
+        CmdDestroy();
+    }
+
+    private void CmdDestroy()
+    {
+        _photonView.RPC(nameof(RpcDestroy), RpcTarget.All);
+    }
+    [PunRPC]
+    private void RpcDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- kitchen_chaos/Assets/_Assets/Scripts/Extension/Extension.cs
+++ kitchen_chaos/Assets/_Assets/Scripts/Extension/Extension.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Mono.CSharp;
 using UnityEngine;
 
@@ -17,5 +18,26 @@ public static class Extension
     public static Vector3 ToVector3XZ(this Vector2 source, float y = 0)
     {
         return new Vector3(source.x, y, source.y);
+    }
+
+}
+
+public static class AnimatorExtension
+{
+    public static float GetCurrentAnimationLength(this Animator animator)
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).length;
+    }
+    public static float GetAnimationLength(this Animator animator, string animationName)
+    {
+        try
+        {
+            return animator.runtimeAnimatorController.animationClips.First(clip => clip.name == animationName).length;
+        }
+        catch
+        {
+            Debug.LogError($"Animation {animationName} not found");
+            return 0;
+        }
     }
 }
