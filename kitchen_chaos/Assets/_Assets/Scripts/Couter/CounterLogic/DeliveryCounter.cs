7dd4e2b98416b9a5e67fe2694d56069acb48c679
Noody (NoodyAbility) <62384988+Nood-the-GD@users.noreply.github.com>
2025-03-18 22:11:19 +0700

    fix(delivery-counter): improve delivery logic and error messages
    - Adjusted the order of checks in the delivery logic to prioritize checking for a plate before other conditions.
    - Improved error messages to be more specific, guiding the player towards the correct actions.

--- kitchen_chaos/Assets/_Assets/Scripts/Couter/CounterLogic/DeliveryCounter.cs
+++ kitchen_chaos/Assets/_Assets/Scripts/Couter/CounterLogic/DeliveryCounter.cs
@@ -31,7 +31,8 @@ public class DeliveryCounter : BaseCounter
                 {
                     message = "Need plate";
                 }
-                else{
+                else
+                {
                     message = "Not in the order list";
                 }
                 CmdShowMessage(message);
